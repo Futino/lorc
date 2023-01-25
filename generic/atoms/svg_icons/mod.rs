9b26e704c689fddeec08cc865c2e24bcc03c6d8e
Jorge Lewis <antopiahk@gmail.com>
2023-01-26 00:42:21 +0800

    arrows too small :'(

--- generic/atoms/svg_icons/mod.rs
+++ generic/atoms/svg_icons/mod.rs
@@ -71,7 +71,7 @@ pub fn SeperatorIcon() -> Html {
 #[function_component]
 pub fn RightArrow() -> Html {
     html! {
-        <svg aria-hidden="true" height="6" width="6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <svg aria-hidden="true" class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7">
         </path>
     </svg>
@@ -81,7 +81,7 @@ pub fn RightArrow() -> Html {
 #[function_component]
 pub fn LeftArrow() -> Html {
     html! {
-        <svg aria-hidden="true" height="6" width="6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <svg aria-hidden="true" class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7">
             </path>
         </svg>
