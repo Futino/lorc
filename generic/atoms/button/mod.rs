7c110c2c0b8e92bbe6fea5e512c0116bcc686936
Jorge Lewis <antopiahk@gmail.com>
2023-01-28 02:30:34 +0800

    Update mod.rs

--- generic/atoms/button/mod.rs
+++ generic/atoms/button/mod.rs
@@ -9,14 +9,14 @@ pub struct Button;
 
 impl Component for Button {
     type Message = ();
-    type Properties = ();
+    type Properties = Props;
 
     fn create(ctx: &Context<Self>) -> Self {
         Self
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
-        let onclick = ctx.props().options.onclick;
+        let onclick = &ctx.props().option.onclick;
         html! {
             <button {onclick} class="inline-flex overflow-hidden relative justify-center items-center py-3 px-6 font-bold rounded-md shadow-2xl group text-primary-on-light dark:text-primary-on-dark hover:text-secondary-light dark:hover:text-secondary-dark">
                 <span class="absolute inset-0 w-full h-full bg-gradient-to-br from-primary-light to-secondary-light dark:from-primary-dark dark:to-secondary-dark opacity-100 transition duration-300 ease-out group-hover:opacity-0 "></span>
