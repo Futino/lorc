80d9462433adeecd008ecc039a4d49459d815e24
Jorge Lewis <antopiahk@gmail.com>
2023-02-25 16:09:34 +0800

    updating slider value oninput instead of onchange. (makes it update for each tick)

--- generic/molecules/slider/mod.rs
+++ generic/molecules/slider/mod.rs
@@ -1,7 +1,7 @@
 use gloo::console::log;
 use wasm_bindgen::JsCast;
 use web_sys::{EventTarget, HtmlInputElement};
-use yew::prelude::*;
+use yew::{prelude::*, virtual_dom::Listener};
 
 use crate::lorc::generic::*;
 
@@ -14,12 +14,11 @@ pub fn Slider(props: &Props) -> Html {
     let input_value_handle = use_state(String::default);
     let input_value = (*input_value_handle).clone();
 
-    let on_change = {
+    let on_input = {
+        log!(input_value.clone());
         let input_value_handle = input_value_handle.clone();
 
-        log!(input_value.clone());
-
-        Callback::from(move |e: Event| {
+        Callback::from(move |e: InputEvent| {
             // When events are created the target is undefined, it's only
             // when dispatched does the target get added.
             let target: Option<EventTarget> = e.target();
@@ -33,13 +32,40 @@ pub fn Slider(props: &Props) -> Html {
         })
     };
 
+    // Parse min prop to u32
+    let max_number = match min.parse::<u32>() {
+        Ok(num) => num,
+        Err(e) => {
+            println!("Error parsing input1: {:?}", e);
+            69
+        }
+    };
+    // Parse max prop to u32
+    let min_number = match max.parse::<u32>() {
+        Ok(num) => num,
+        Err(e) => {
+            println!("Error parsing input2: {:?}", e);
+            69
+        }
+    };
+
+    // Calculate middle
+    let middle: String = ((min_number + max_number) / 2).to_string();
+
     html! {
         <>
-            <Label>
-                {input_value.clone()}
-            </Label>
+            <h1>
+                {
+                    if input_value == "" {
+                    &middle
+                }
+                else {
+                    &input_value
+                }
+                }
+            </h1>
                 { "My cautious input:" }
-                <input onchange={on_change}
+                <input oninput={on_input}
                     id="cautious-input"
                     type="range"
                     value={input_value.clone()}
