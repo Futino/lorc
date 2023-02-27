c3a53b88017cabdada6db6d0cb1f0d33efafcc17
Jorge Lewis <antopiahk@gmail.com>
2023-02-27 15:14:54 +0800

    text_link removed old label, added class prop to button component

--- generic/atoms/button/props.rs
+++ generic/atoms/button/props.rs
@@ -4,7 +4,9 @@ use super::ButtonOptions;
 use yew::{Children, Properties};
 
 #[derive(Properties, PartialEq)]
-pub struct Props{
+pub struct Props {
+    #[prop_or(String::new())]
+    pub class: String,
     #[prop_or_default]
     pub children: Children,
     pub options: ButtonOptions,
--- generic/atoms/text_link/mod.rs
+++ generic/atoms/text_link/mod.rs
@@ -9,11 +9,9 @@ use crate::lorc::generic::atoms::*;
 pub fn TextLink(props: &Props) -> Html {
     let Props { children, href } = props;
     html! {
-    <Label>
-                            <a class="inline text-secondary-light dark:text-secondary-dark hover:text-tertiary-light dark:hover:text-tertiary-dark"
-                                href={href.to_owned()}>
-                                {for children.iter()}
-                            </a>
-                    </Label>
-        }
+                        <a class="inline text-secondary-light dark:text-secondary-dark hover:text-tertiary-light dark:hover:text-tertiary-dark"
+                            href={href.to_owned()}>
+                            {for children.iter()}
+                        </a>
+    }
 }
