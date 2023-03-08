cb8df3ca8c3a761eedec1862b2257388471e28ea
Jorge Lewis <antopiahk@gmail.com>
2023-03-08 21:18:52 +0800

    atoms conversion

--- src/lib/generic/atoms/Text.svelte
+++ src/lib/generic/atoms/Text.svelte
@@ -1,60 +1,18 @@
 <script lang="ts">
-	enum TextType {
-		Display,
-		Headline,
-		Title,
-		Label,
-		Body
-	}
-	enum TextSize {
-		Large,
-		Medium,
-		Small
-	}
+
+    // I'll let you handle the rest since idk what you mean by your shortcut.
+    type textSize = 'large' | 'medium' | 'small'
+    type textType = 'display' | 'headline' | 'title' | 'label' | 'body'
+
+    type StyledText = `${textType}-${textSize}`;
+
+    const magic:StyledText = "label-small";
 
 	//export let textType;
 	//export let textSize;
-
-    let className = "";
-    export { className as class};
-
-    /*
-    let className = "";
-	
-        switch(textType.type) {
-        case TextType.Display:
-            className += "display-";
-            break;
-        case TextType.Headline:
-        className += "headline-";
-            break;
-        case TextType.Title:
-        className += "title-";
-            break;
-        case TextType.Label:
-        className += "label-";
-            break;
-        case TextType.Body:
-        className += "body-";
-            break;
-    };
-    
-   
-    switch(textSize) {
-        case TextSize.Large:
-        className += "large";
-            break;
-        case TextSize.Medium:
-        className += "medium";
-            break;
-        case TextSize.Small:
-        className += "small";
-            break;
-    }
-    */
 </script>
 
 
-<div id="text" class={className}>
+<div id="text" class="{$$props.class}">
 	<slot />
 </div>
