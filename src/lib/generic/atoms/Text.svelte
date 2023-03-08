12418ea1d0d7fe60291f199aa6cf7c2083b6889f
Jorge Lewis <antopiahk@gmail.com>
2023-03-08 13:40:38 +0800

    starting migration by setting up svelte, and converting text rust to text svelte

---
+++ src/lib/generic/atoms/Text.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+	enum TextType {
+		Display,
+		Headline,
+		Title,
+		Label,
+		Body
+	}
+	enum TextSize {
+		Large,
+		Medium,
+		Small
+	}
+
+	//export let textType;
+	//export let textSize;
+
+    let className = "";
+    export { className as class};
+
+    /*
+    let className = "";
+	
+        switch(textType.type) {
+        case TextType.Display:
+            className += "display-";
+            break;
+        case TextType.Headline:
+        className += "headline-";
+            break;
+        case TextType.Title:
+        className += "title-";
+            break;
+        case TextType.Label:
+        className += "label-";
+            break;
+        case TextType.Body:
+        className += "body-";
+            break;
+    };
+    
+   
+    switch(textSize) {
+        case TextSize.Large:
+        className += "large";
+            break;
+        case TextSize.Medium:
+        className += "medium";
+            break;
+        case TextSize.Small:
+        className += "small";
+            break;
+    }
+    */
+</script>
+
+
+<div id="text" class={className}>
+	<slot />
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Text from "$lib/generic/atoms/Text.svelte";
+</script>
+<h1>Welcome to LORC</h1>
+<p>Look for the component you need.</p>
+<p>If it's not here... make it!</p>
+
+<h1> The Examples of each component are displayed with respective css to LORC. Most css should be the same. But somethings, like colors, will most likely not be.</h1>
+
+<Text class="display-large">
+    Hello there
+</Text>
